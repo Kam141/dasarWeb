6ef888ed0420e56fd9d60f30d26ef347770ff540
Kam141 <kamilafadhil@gmail.com>
2024-09-18 21:41:07 +0700

    praktikum 4

--- minggu4/variabel_konstanta.php
+++ minggu4/variabel_konstanta.php
@@ -2,11 +2,11 @@
 $angka1 = 10;
 $angka2 = 5;
 $hasil = $angka1+$angka2;
-echo "Hasil penjumlahan $angka1 dan $angka2 adalah $hasil.". "\n";
+echo "Hasil penjumlahan $angka1 dan $angka2 adalah $hasil.". "<br>";
 
 $benar = true;
 $salah = false;
-echo "variabel benar: $benar, variabel salah: $salah"."\n";
+echo "variabel benar: $benar, variabel salah: $salah"."<br>";
 
 define("NAMA_SITUS", "WebsiteKu.com");
 define("Tahun_Pendirian", 2023);
