38e1e7ae85ca28bc2c706bdff804b7b18b83dd75
Kam141 <kamilafadhil@gmail.com>
2024-09-19 19:45:25 +0700

    praktikum 4 (pembenaran)

--- minggu4/struktur_kontrol.php
+++ minggu4/struktur_kontrol.php
@@ -31,5 +31,58 @@ $jumlahBuah = 0;
 for ($i = 1; $i <= $jumlahLahan; $i++) {
     $jumlahBuah += ($tanamanPerlahan * $buahPerTanaman);
 }
-echo "Jumlah buah yang akan dipanen adalah $jumlahBuah";
+echo "Jumlah buah yang akan dipanen adalah $jumlahBuah <br><br>";
+
+$skorUjian = [85, 92, 78, 96, 88];
+$totalSkor = 0;
+
+foreach ($skorUjian as $skor ) {
+    $totalSkor += $skor;
+}
+echo "Total skor ujian adalah $totalSkor<br><br>";
+
+$nilaiMahasiswa = [85, 92, 58, 64, 90, 55, 88, 79, 70, 96];
+foreach ($nilaiMahasiswa as $nilai) {
+    if ($nilai < 60) {
+        echo "Nilai: $nilai (Tidak Lulus) <br>";
+        continue;
+    }
+    echo "Nilai: $nilai (Lulus) <br>";
+}
+echo "<br>";
+
+// Tugas 1
+$nilaiMahasiswa2 = [85, 92, 58, 64, 90, 55, 88, 79, 70, 96];
+
+sort($nilaiMahasiswa2);
+
+$nilai = array_slice ($nilaiMahasiswa2, 2, count($nilaiMahasiswa2) - 4);
+$rata = array_sum($nilai) / count($nilai);
+
+echo "Nilai rata-rata kelas: $rata <br><br>";
+
+
+// Tugas 2
+echo "<br>";
+$hargaProduk = 120000;
+$diskon = $hargaProduk * 0.2;
+$bayar = $hargaProduk -$diskon;
+echo  "Yang harus dibayar oleh pelanggan: $bayar <br><br>";
+
+// tugas 3
+$poinSeorang=[100,200,300, 400];
+$totalPoin =0;
+
+foreach ($poinSeorang as $poin) {
+    $totalPoin += $poin;
+}
+
+echo "Total skor pemain adalah: $totalPoin <br>";
+
+if ($totalPoin > 500) {
+    echo "Apakah pemain mendapatkan mendapatkan hadiah? Ya <br>";
+} else {
+    echo "Apakah pemain mendapatkan mendapatkan hadiah? Tidak <br>";
+}
+
 ?>
