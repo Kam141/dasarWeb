106cc08497eec6888e8c3977950a7c4d98f1190d
Kam141 <kamilafadhil@gmail.com>
2024-09-23 17:02:50 +0700

    soal 7

--- minggu5/rekursif.php
+++ minggu5/rekursif.php
@@ -1,8 +1,17 @@
 <?php
-function tampilkanHaloDunia() {
-    echo "Hello dunia! <br>";
-    tampilkanHaloDunia();
+// function tampilkanHaloDunia() {
+//     echo "Hello dunia! <br>";
+//     tampilkanHaloDunia();
+// }
+
+// tampilkanHaloDunia();
+function tampilkanAngka(int $jumlah, int $indeks = 1){
+echo "Perulangan ke-{$indeks} <br>";
+
+    if($indeks < $jumlah){
+        tampilkanAngka($jumlah, $indeks + 1);
+    }
 }
 
-tampilkanHaloDunia();
+tampilkanAngka(20);
 ?>
