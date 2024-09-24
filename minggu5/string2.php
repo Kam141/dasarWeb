bb445bfda7c0867809f39f7cb33fff94d787f551
Kam141 <kamilafadhil@gmail.com>
2024-09-24 22:24:25 +0700

    soal 10

---
+++ minggu5/string2.php
@@ -0,0 +1,13 @@
+<?php
+echo "Baris\nbaru <br>";
+echo 'Baris\nbaru <br>';
+echo "Hallo\rDunia <br>";
+echo 'Hallo\rDunia <br>';
+echo "Hallo\nDunia <br>";
+
+echo "<pre>Halo\tDunia!<br></pre>";
+echo '<pre>Halo\tDunia!<br></pre>';
+
+echo "Katakanlah \"Tidak pada narkoba!\" <br>";
+echo 'Katakanlah \'Tidak pada narkoba!\' <br>';
+?>
