e1a1c6f9e97e2e05208ed34a488deed1413a40f8
Kam141 <kamilafadhil@gmail.com>
2024-09-24 22:04:13 +0700

    soal 8

--- minggu5/rekursif.php
+++ minggu5/rekursif.php
@@ -5,13 +5,60 @@
 // }
 
 // tampilkanHaloDunia();
-function tampilkanAngka(int $jumlah, int $indeks = 1){
-echo "Perulangan ke-{$indeks} <br>";
+// function tampilkanAngka(int $jumlah, int $indeks = 1){
+// echo "Perulangan ke-{$indeks} <br>";
 
-    if($indeks < $jumlah){
-        tampilkanAngka($jumlah, $indeks + 1);
+//     if($indeks < $jumlah){
+//         tampilkanAngka($jumlah, $indeks + 1);
+//     }
+// }
+
+// tampilkanAngka(20);
+
+    $menu = [
+        [
+            "nama" => "beranda"
+        ],
+        [
+            "nama" => "Berita",
+            "subMenu" => [
+                [
+                    "nama" => "Wisata",
+                    "subMenu" => [
+                        [
+                            "nama" => "Pantai"
+                        ],
+                        [
+                            "nama" => "Gunung"
+                        ]
+                    ]
+                ],
+                [
+                    "nama" => "kuliner"
+                ],
+                [
+                    "nama" => "hiburan"
+                ]
+            ]
+        ],
+        [
+            "nama" => "Tentang"        
+        ],
+        [
+            "nama" => "Kontak"
+        ],
+    ];
+
+function tampikanMenuBertingkat (array $menu){
+    echo "<ul>";
+    foreach($menu as $key => $item){
+        echo "<li> {$item['nama']}";
+        if (isset($item['subMenu'])) {
+            tampikanMenuBertingkat($item['subMenu']);
+        }
     }
+    echo "</ul>";
 }
 
-tampilkanAngka(20);
+tampikanMenuBertingkat($menu);
 ?>
