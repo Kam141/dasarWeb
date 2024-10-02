88f2afd62ae49213c2089a8fda2d3c8f33fda3b0
Kam141 <kamilafadhil@gmail.com>
2024-10-02 10:54:57 +0700

    soal 4

---
+++ minggu6/praktik_php/fungsi.php
@@ -0,0 +1,10 @@
+<?php
+
+function perkenalan(){
+    echo "Assalamualaikum, ";
+    echo "Perkenalkan, nama saya Kamila<br>";
+    echo "Senang berkenalan dengan Anda<br>";
+}
+perkenalan();
+perkenalan();
+?>
