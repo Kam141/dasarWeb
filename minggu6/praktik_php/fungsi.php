8db601e92edc6f3c6bc1915d82e6dd1836244173
Kam141 <kamilafadhil@gmail.com>
2024-10-02 11:11:39 +0700

    soal 7

--- minggu6/praktik_php/fungsi.php
+++ minggu6/praktik_php/fungsi.php
@@ -11,5 +11,10 @@ $saya = "Elok";
 $ucapanSalam = "Selamat Pagi";
 perkenalan($saya);
 
-
+echo "<hr>";
+function hitungUmur($thn_lahir, $thn_sekarang) {
+    $umur = $thn_sekarang - $thn_lahir;
+    return $umur;
+}
+echo "Umur saya adalah ". hitungUmur(1988, 2023) . " tahun<br><br>";
 ?>
