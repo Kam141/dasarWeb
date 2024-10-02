8e14cad14f98dc8c575ac5c62bde67d06d68f3f2
Kam141 <kamilafadhil@gmail.com>
2024-10-02 11:11:02 +0700

    soal 6

--- minggu6/praktik_php/fungsi.php
+++ minggu6/praktik_php/fungsi.php
@@ -1,6 +1,6 @@
 <?php
 
-function perkenalan($nama, $salam){
+function perkenalan($nama, $salam="Assalamualaikum"){
     echo $salam. ", ";
     echo "Perkenalkan, nama saya " . $nama . "<br>";
     echo "Senang berkenalan dengan Anda<br>";
@@ -9,5 +9,7 @@ perkenalan("Hamdana", "Hallo");
 echo "<hr>";
 $saya = "Elok";
 $ucapanSalam = "Selamat Pagi";
-perkenalan($saya, $ucapanSalam);
+perkenalan($saya);
+
+
 ?>
