b965389b03ab45e7ca2e1d678ae89b14df36eb31
Kam141 <kamilafadhil@gmail.com>
2024-10-02 11:35:58 +0700

    soal 12

--- minggu6/praktik_php/rekursif.php
+++ minggu6/praktik_php/rekursif.php
@@ -6,12 +6,56 @@
     
 // tampilkanHaloDunia();
 
-function tampilkanAngka(int $jumlah, int $indeks = 1){
-    echo "Perulangan ke-{$indeks} <br>";
+// function tampilkanAngka(int $jumlah, int $indeks = 1){
+//     echo "Perulangan ke-{$indeks} <br>";
 
-    if($indeks < $jumlah){
-        tampilkanAngka($jumlah, $indeks + 1);
-    }
+//     if($indeks < $jumlah){
+//         tampilkanAngka($jumlah, $indeks + 1);
+//     }
+// }
+// tampilkanAngka(20);
+
+$menu = [
+    [
+        "nama" => "Beranda"
+    ],
+    [
+        "nama" => "Berita",
+        "subMenu" => [
+            [
+                "nama" => "Wisata",
+                "subMenu" => [
+                    [
+                        "nama" => "Pantai"
+                    ],
+                    [
+                        "nama" => "Gunung"
+                    ]
+                ]
+            ],
+            [
+                "nama" => "kuliner"
+            ],
+            [
+                "nama" => "hiburan"
+            ]
+        ]
+    ],
+    [
+        "nama" => "Tentang"        
+    ],
+    [
+        "nama" => "Kontak"
+    ],
+];
+
+function tampikanMenuBertingkat (array $menu){
+echo "<ul>";
+foreach($menu as $key => $item){
+    echo "<li> {$item['nama']} </li>";
 }
-tampilkanAngka(20);
+echo "</ul>";
+}
+
+tampikanMenuBertingkat($menu);
 ?>
