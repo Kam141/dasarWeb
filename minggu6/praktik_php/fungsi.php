f770fb794aa139496297a19baac4735fe556d0e8
Kam141 <kamilafadhil@gmail.com>
2024-10-02 11:25:55 +0700

    soal 8

--- minggu6/praktik_php/fungsi.php
+++ minggu6/praktik_php/fungsi.php
@@ -1,20 +1,24 @@
 <?php
 
-function perkenalan($nama, $salam="Assalamualaikum"){
-    echo $salam. ", ";
-    echo "Perkenalkan, nama saya " . $nama . "<br>";
-    echo "Senang berkenalan dengan Anda<br>";
-}
-perkenalan("Hamdana", "Hallo");
-echo "<hr>";
-$saya = "Elok";
-$ucapanSalam = "Selamat Pagi";
-perkenalan($saya);
-
-echo "<hr>";
 function hitungUmur($thn_lahir, $thn_sekarang) {
     $umur = $thn_sekarang - $thn_lahir;
     return $umur;
 }
-echo "Umur saya adalah ". hitungUmur(1988, 2023) . " tahun<br><br>";
+function perkenalan($nama, $salam="Assalamualaikum"){
+    echo $salam. ", ";
+    echo "Perkenalkan, nama saya " . $nama . "<br>";
+
+    echo "Saya berusia " . hitungUmur(1988, 2023) . " tahun<br>";
+    echo "Senang berkenalan dengan Anda<br>";
+}
+// perkenalan("Hamdana", "Hallo");
+// echo "<hr>";
+// $saya = "Elok";
+// $ucapanSalam = "Selamat Pagi";
+// perkenalan($saya);
+
+// echo "<hr>";
+// echo "Umur saya adalah ". hitungUmur(1988, 2023) . " tahun<br><br>";
+
+perkenalan("Elok");
 ?>
