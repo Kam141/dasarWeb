54338dca1437691c039db61b9a81eb2d50792902
Kam141 <kamilafadhil@gmail.com>
2024-10-02 11:29:32 +0700

    soal 10

--- minggu6/praktik_php/rekursif.php
+++ minggu6/praktik_php/rekursif.php
@@ -1,11 +1,17 @@
 <?php
-function tampilkanHaloDunia() {
-    echo "Hello dunia! <br>";
-    tampilkanHaloDunia();
-}
-    
-tampilkanHaloDunia();
-// for ($i=1; $i <= 25 ; $i++) { 
-//     echo "Perulangan ke-{$i} <br>"; 
+// function tampilkanHaloDunia() {
+//     echo "Hello dunia! <br>";
+//     tampilkanHaloDunia();
 // }
+    
+// tampilkanHaloDunia();
+
+function tampilkanAngka(int $jumlah, int $indeks = 1){
+    echo "Perulangan ke-{$indeks} <br>";
+
+    if($indeks < $jumlah){
+        tampilkanAngka($jumlah, $indeks + 1);
+    }
+}
+tampilkanAngka(20);
 ?>
