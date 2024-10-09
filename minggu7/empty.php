dcbf2db72b08d25bada26c905a179a292bc179c2
Kam141 <kamilafadhil@gmail.com>
2024-10-09 09:32:33 +0700

    soal 2.2

--- minggu7/empty.php
+++ minggu7/empty.php
@@ -6,4 +6,11 @@
         echo "Array terdefinisi dan tidak kosong";
     }
     
+    echo "<br><br>";
+    if (empty($nonExistenVar)) {
+        echo "Variabel tidak terdefinisi atau kosong";
+    } else {
+        echo "Variabel terdefinisi dan tidak kosong";
+    }
+    
 ?>
