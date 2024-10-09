9a3ac6051c616256f51af3a60015a2f66ce193bd
Kam141 <kamilafadhil@gmail.com>
2024-10-10 06:37:05 +0700

    soal 5.3

--- minggu7/regex.php
+++ minggu7/regex.php
@@ -8,13 +8,21 @@
         echo "Tidak ada huruf kecil";
     }*/
 
-    $pattern = '/[0-9]+/';
+    /* $pattern = '/[0-9]+/';
     $text = 'There are 123 apples.';
 
     if (preg_match($pattern, $text, $matches)) {
         echo "Cocokkan: ". $matches[0];
     } else {
         echo "Tidak ada yang cocok";
-    }
+    } */
+    
+    $pattern = '/apple/';
+    $replcement = 'banana';
+    $text = 'I like apple pie.';
+    $newtext = preg_replace($pattern, $replcement, $text);
+    echo $newtext;
+
+    
     
 ?>
