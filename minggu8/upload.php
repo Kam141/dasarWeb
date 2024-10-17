6239f824f3b6b1ddcbd93a29555b73a960d74d8b
Kam141 <kamilafadhil@gmail.com>
2024-10-17 08:07:09 +0700

    soal 4 dan 5

--- minggu8/upload.php
+++ minggu8/upload.php
@@ -9,7 +9,9 @@
 
         if (in_array($fileType, $allowedExtensions) && $_FILES['myfile']['size'] <= $maxsize) {
             if (move_uploaded_file($_FILES['myfile']['tmp_name'], $targetfile)) {
-                echo "File berhasil diunggah."; 
+                echo "File berhasil diunggah.";
+                echo "<br>";
+                echo "<img src='$targetfile' width='200' height='auto' alt='Thumbnail'>";
             } else {
                 echo "Gagal mengunggah file.";
             }
